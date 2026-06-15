997cd3db9958f5761759813c15a828e86115bec4
Gurmeet Punia <gurmeetpunia404@gmail.com>
2026-06-15 20:26:13 +0530

    fixed merge conflict in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import numpy as np
 import joblib
@@ -9,15 +8,15 @@ model = joblib.load("titanic_model.pkl")
 st.title("🚢 Titanic Survival Predictor")
 
 # inputs
-pclass = st.selectbox("Pclass", [1,2,3])
-sex = st.selectbox("Sex", ["male","female"])
+pclass = st.selectbox("Pclass", [1, 2, 3])
+sex = st.selectbox("Sex", ["male", "female"])
 age = int(st.number_input("Age"))
 fare = st.number_input("Fare")
-embarked = st.selectbox("Embarked", ["S","C","Q"])
+embarked = st.selectbox("Embarked", ["S", "C", "Q"])
 
 # encoding
 sex = 0 if sex == "male" else 1
-embarked = {"S":0,"C":1,"Q":2}[embarked]
+embarked = {"S": 0, "C": 1, "Q": 2}[embarked]
 
 # prediction
 if st.button("Predict"):
@@ -27,34 +26,4 @@ if st.button("Predict"):
     if prediction[0] == 1:
         st.success("🎉 Survived")
     else:
-=======
-import streamlit as st
-import numpy as np
-import joblib
-
-# load model
-model = joblib.load("titanic_model.pkl")
-
-st.title("🚢 Titanic Survival Predictor")
-
-# inputs
-pclass = st.selectbox("Pclass", [1,2,3])
-sex = st.selectbox("Sex", ["male","female"])
-age = int(st.number_input("Age"))
-fare = st.number_input("Fare")
-embarked = st.selectbox("Embarked", ["S","C","Q"])
-
-# encoding
-sex = 0 if sex == "male" else 1
-embarked = {"S":0,"C":1,"Q":2}[embarked]
-
-# prediction
-if st.button("Predict"):
-    input_data = np.array([[pclass, sex, age, fare, embarked]])
-    prediction = model.predict(input_data)
-
-    if prediction[0] == 1:
-        st.success("🎉 Survived")
-    else:
->>>>>>> 98ef076f6e063f5d120459537555a30116b3bc7e
         st.error("💀 Not Survived")
