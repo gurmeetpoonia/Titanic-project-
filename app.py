110661f5a1e872d7564fbf5949bcc495ab32ed4f
Gurmeet Punia <gurmeetpunia404@gmail.com>
2026-06-16 12:09:51 +0530

    Updated Titanic project

--- app.py
+++ app.py
@@ -8,22 +8,46 @@ model = joblib.load("titanic_model.pkl")
 st.title("🚢 Titanic Survival Predictor")
 
 # inputs
-pclass = st.selectbox("Pclass", [1, 2, 3])
-sex = st.selectbox("Sex", ["male", "female"])
-age = int(st.number_input("Age"))
+pclass = st.selectbox("Pclass", ["Select",1, 2, 3])
+sex = st.selectbox("Sex", ["Select","male", "female"])
+age = st.number_input("Age",min_value=0, step=1)
 fare = st.number_input("Fare")
-embarked = st.selectbox("Embarked", ["S", "C", "Q"])
+embarked = st.selectbox("Embarked", ["Select","S", "C", "Q"])
 
 # encoding
-sex = 0 if sex == "male" else 1
-embarked = {"S": 0, "C": 1, "Q": 2}[embarked]
+
 
 # prediction
 if st.button("Predict"):
-    input_data = np.array([[pclass, sex, age, fare, embarked]])
-    prediction = model.predict(input_data)
+    if age <= 0:
+        st.warning("Please enter Age")
 
-    if prediction[0] == 1:
-        st.success("🎉 Survived")
+    elif fare <= 0:
+        st.warning("Please enter Fare")
+
+    elif (
+        pclass == "Select" or
+        sex == "Select" or
+        embarked == "Select" 
+    ):
+        st.warning("Please fill all dropdown fields")
+
     else:
-        st.error("💀 Not Survived")
+        # encoding
+        sex = 1 if sex == "female" else 0
+    
+
+        embarked = {
+            "S": 0,
+            "C": 1,
+            "Q": 2
+        }[embarked]
+
+
+        input_data = np.array([[pclass, sex, age, fare, embarked]])
+        prediction = model.predict(input_data)
+
+        if prediction[0] == 1:
+            st.success("🎉 Survived")
+        else:
+            st.error("💀 Not Survived")
